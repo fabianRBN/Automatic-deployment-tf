861e2c791034d56e29a98c76ae6177dfd502f4dc
ftoapanta <fabianRBN_95@hotmail.com>
2023-12-24 18:45:31 -0500

    FIX: Actualizacion de credenciales

--- Jenkisfile.groovy
+++ Jenkisfile.groovy
@@ -6,8 +6,8 @@ pipeline {
     environment {
         // Define las variables de entorno para las credenciales
         // Reemplaza 'id-de-tus-credenciales' con el ID que asignaste a tus credenciales en Jenkins
-        AWS_ACCESS_KEY_ID = credentials('id-de-tus-credenciales-access')
-        AWS_SECRET_ACCESS_KEY = credentials('id-de-tus-credenciales-secret')
+        AWS_ACCESS_KEY_ID = credentials('access_key')
+        AWS_SECRET_ACCESS_KEY = credentials('secret_key')
     }
 
     stages {
