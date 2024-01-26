20af481f5a002ff0684f221f4795ca155fce4ef1
fabianrbn <fabianRBN_95@hotmail.com>
2024-01-26 01:19:57 -0500

    FTO: Generacion de Terraform-aws-testing

---
+++ Jenkisfile-testing.groovy
@@ -0,0 +1,41 @@
+pipeline {
+    agent {
+        label 'terraform'
+    }
+
+    environment {
+        // Define las variables de entorno para las credenciales
+        // Reemplaza 'id-de-tus-credenciales' con el ID que asignaste a tus credenciales en Jenkins
+        AWS_ACCESS_KEY_ID = credentials('access_key')
+        AWS_SECRET_ACCESS_KEY = credentials('secret_key')
+    }
+
+    stages {
+        stage('PWD') {
+            steps {
+                sh 'pwd'
+                sh 'ls -al /var/jenkins/workspace/Terraform-aws-testing' 
+            }
+        }
+        stage('Ejecutar Terraform') {
+            steps {
+        
+                dir('/var/jenkins/workspace/Terraform-aws-testing/environments/testing') {
+                    script {
+                        // Configura las credenciales de AWS como variables de entorno
+                        sh 'export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID'
+                        sh 'export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY'
+
+                        // Ejecuta Terraform
+                        sh 'terraform init'
+                        sh 'terraform plan'
+                        sh 'terraform apply -auto-approve'
+                        sleep time: 120, unit: 'SECONDS'
+                        sh 'terraform destroy -auto-approve'
+
+                    }
+                }
+            }
+        }
+    }
+}
