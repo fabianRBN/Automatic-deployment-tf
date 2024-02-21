3a1fe1b9808974355976ea250e0bb3a004654407
fabianrbn <fabianRBN_95@hotmail.com>
2024-02-20 23:10:04 -0500

    Terminal FTO: Cambio de tiempo de delay 6

--- Jenkisfile.groovy
+++ Jenkisfile.groovy
@@ -30,7 +30,7 @@ pipeline {
                         sh 'terraform init'
                         sh 'terraform plan'
                         sh 'terraform apply -auto-approve'
-                        sleep time: 40, unit: 'SECONDS'
+                        sleep time: 60, unit: 'SECONDS'
                         sh 'terraform destroy -auto-approve'
 
                     }
